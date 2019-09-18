05b3cf616aaf04005507fe2a4824b1754884695c
ina-uzu <ina.uzu.song@gmail.com>
2019-09-18 17:07:35 +0900

    team & contact page added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET team page */
+router.get('/team', function(req,res){
+  res.render('team');
+});
+
+/* GET contact page */
+router.get('/contact', function(req,res){
+  res.render('contact');
+});
+
+
 module.exports = router;
